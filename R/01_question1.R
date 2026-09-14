a2a3107722735bf0de05feccd81c23ae879d4bd9
fernandasreis <feernandasilveirareis@gmail.com>
2026-09-14 11:38:33 -0300

    Fix indexing for data_group selection in R script

--- R/01_question1.R
+++ R/01_question1.R
@@ -1,9 +1,9 @@
 matriculas <- c(582795, 578061, 579540) 
 M <- max(matriculas) 
-r <- 1 + (M %% 100) 
+r <- 1 + (M %% 100) #96
 
-s <- r + 300 - 1 
-data_group <- HW1_bike_sharing[96:395, ] 
+s <- r + 300 - 1 #395
+data_group <- HW1_bike_sharing[r:s, ] 
 
 firstdate <- data_group$dteday[1] 
 lastdate <- data_group$dteday[300]
