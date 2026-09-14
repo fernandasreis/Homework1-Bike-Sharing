6fc2a9d8b3278e8c481467dc9e1d8cec91b191ea
fernandasreis <feernandasilveirareis@gmail.com>
2026-09-14 11:29:06 -0300

    Add code defining data_group

--- R/01_question1.R
+++ R/01_question1.R
@@ -1,3 +1,9 @@
 matriculas <- c(582795, 578061, 579540) 
 M <- max(matriculas) 
 r <- 1 + (M %% 100) 
+
+s <- r + 300 - 1 
+data_group <- HW1_bike_sharing[96:395, ] 
+
+firstdate <- data_group$dteday[1] 
+lastdate <- data_group$dteday[300]
