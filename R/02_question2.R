825295a303512fa1b4081066bfdb36a8fcfac38e
fernandasreis <feernandasilveirareis@gmail.com>
2026-09-16 09:45:45 -0300

    Create mode function and data_group10
    Added modefunc to calculate the mode of a vector.

--- R/02_question2.R
+++ R/02_question2.R
@@ -1,1 +1,12 @@
-codigo usado na questao 2
+modefunc <- function(x){ 
+  valores_unicos <- unique(x) 
+  posicoes <- match(x,valores_unicos) 
+  posi_frequencia <- tabulate(posicoes) 
+  posi_moda <- which.max(posi_frequencia) 
+  valor_moda <- valores_unicos[posi_moda] 
+  
+  return(valor_moda) 
+}
+
+#PARA VERIFICAÇÃO DOS CÁLCULOS MANUAIS
+data_group10 <- data_group[1:10, ]
