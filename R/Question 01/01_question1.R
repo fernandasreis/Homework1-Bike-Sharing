c113b26118e96307ff7aab3506f119752b1361f3
fernandasreis <feernandasilveirareis@gmail.com>
2026-09-16 10:46:46 -0300

    Add comments for firstdate and lastdate variables

--- R/Question 01/01_question1.R
+++ R/Question 01/01_question1.R
@@ -5,5 +5,5 @@ r <- 1 + (M %% 100) #96
 s <- r + 300 - 1 #395
 data_group <- HW1_bike_sharing[r:s, ] 
 
-firstdate <- data_group$dteday[1] 
-lastdate <- data_group$dteday[300]
+firstdate <- data_group$dteday[1] #2011-04-06
+lastdate <- data_group$dteday[300] #2012-01-30
