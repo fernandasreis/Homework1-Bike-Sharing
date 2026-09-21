ddc5bfa5e6cd3556441a275b616949336ea50d6e
fernandasreis <feernandasilveirareis@gmail.com>
2026-09-21 09:35:10 -0300

    Add comments to date conversion and plotting code

--- R/Question 04/4_1_question04.R
+++ R/Question 04/4_1_question04.R
@@ -1,9 +1,9 @@
-data_group$dteday <- as.Date(data_group$dteday)
+data_group$dteday <- as.Date(data_group$dteday) #converte dteday para um formato de data reconhecida pelo R
 
 plot(data_group$dteday, data_group$total_user,
      type = "l",
      xlab = "Data",
      ylab = "Total de usuários",
-     main = "Série temporal de total_user")
+     main = "Série temporal de total_user") #gráfico
 
-abline(h = Q1, lty = 2)
+abline(h = Q1, lty = 2) #linha do quartil 1
